9ddb749963e533cd60b73c0f1288bd6caf8bdd2f
Dennis <dennischacon8@gmail.com>
2025-09-21 21:23:14 -0400

    feat(noti): local notification permission and demo buttons

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -8,6 +8,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
-export class Home {}
+export class Home {
+  requestPermission() {
+    if ('Notification' in window) {
+      Notification.requestPermission().then(permission => {
+        console.log('Notification permission:', permission);
+      });
+    }
+  }
 
-
+  showNotification() {
+    if ('Notification' in window && Notification.permission === 'granted') {
+      new Notification('¡Notificación activada!', {
+        body: 'Esta es una prueba desde tu app Angular.',
+        icon: 'assets/icons/icon-144x144.png'
+      });
+    }
+  }
+}
